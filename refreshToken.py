75adf87f268be0ead075183601e59bbf91ac0f36
Erica Ching <eching11@gmail.com>
2019-10-25 15:38:28 -0700

    Updated

--- refreshToken.py
+++ refreshToken.py
@@ -13,13 +13,3 @@ print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 print("  Copy and Paste token into Filemaker:\n")
 print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 print(j["access_token"])
-
-
-
-# SAMPLE 
-POST https://account-d.docusign.com/oauth/token
-Authorization: Basic BASE64_COMBINATION_OF_INTEGRATION_AND_SECRET_KEYS
-
-grant_type=refresh_token&refresh_token=YOUR_REFRESH_TOKEN
-
-curl --header "Authorization: Basic MjMwNTQ2.....Y4MmM3YmYyNzZlOQ==" --data "grant_type=refresh_token&refresh_token=ey4fd.....3d31d` --request POST https://account-d.docusign.com/oauth/token
